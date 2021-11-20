de01fba40c46f84430c8bb375ff546bf44f895be
ice805 <81813325+ice805@users.noreply.github.com>
2021-11-20 20:21:49 +0000

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,12 @@
+function discord(url) {
+    window.open('https://discord.gg/WayU6WSTW7', '_blank');
+    }
+function page1(url) {
+    window.open('page1.html', '_blank');
+    }
+function page2(url) {
+    window.open('page2.html', '_blank');
+    }
+function page3unreleased(url) {
+    window.open('page3unreleased.html');
+    }
